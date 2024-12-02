003f7e10456acc0008600af2c3cd31b7445f2f1b
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2024-12-02 08:33:51 +0100

    update 1.feladat

---
+++ 1_feladat.py
@@ -0,0 +1,10 @@
+import random
+import math
+
+numbers = [random.randint(-60, 100) for _ in range(50)]
+
+print("1. Mennyi a sorozatban található számok szorzata?")
+product_of_numbers = math.prod(numbers)
+print("A számok szorzata:", product_of_numbers)
+
+print("Alap lista:", numbers)
