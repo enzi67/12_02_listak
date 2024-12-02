196de6dfecb6c95a9424a9b784b248cab004f0b3
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2024-12-02 08:42:54 +0100

    3. feladat

---
+++ 3_feladat.py
@@ -0,0 +1,14 @@
+import random
+
+numbers = [random.randint(-60, 100) for _ in range(50)]
+
+print("3. Írjuk ki az első 3-mal és 7-tel osztható szám indexét!")
+
+for i, num in enumerate(numbers):
+    if num % 3 == 0 and num % 7 == 0:
+        print(f"Az első 3-mal és 7-tel osztható szám indexe: {i}")
+        break
+else:
+    print("Nincs 3-mal és 7-tel osztható szám a listában.")
+
+print("Alap lista:", numbers)
