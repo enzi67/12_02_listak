8c89249eb3ff633f147fa973c10afa472d5aaa90
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2024-12-02 09:20:01 +0100

    szunet

---
+++ 6_feladat.py
@@ -0,0 +1,11 @@
+import random
+
+numbers = [random.randint(-60, 100) for _ in range(50)]
+
+print("6. Hány 18-cal osztható szám található a sorozatban?")
+
+count = sum(1 for num in numbers if num % 18 == 0)
+
+print(f"Az 18-cal osztható számok száma: {count}")
+
+print("Alap lista:", numbers)
