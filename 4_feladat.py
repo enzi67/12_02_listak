a625e59ace4edfe4b7b64a8458edaca4d426203b
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2024-12-02 08:54:15 +0100

    4.feladat

---
+++ 4_feladat.py
@@ -0,0 +1,19 @@
+import random
+
+numbers = [random.randint(-60, 100) for _ in range(50)]
+
+print("4. Igaz-e, hogy minden szám negatív?")
+
+minden_negativ = True
+
+for num in numbers:
+    if num >= 0:
+        minden_negativ = False
+        break
+
+if minden_negativ:
+    print("Igen, minden szám negatív. Nincs pozitív szám a listában.")
+else:
+    print("Nem minden szám negatív. Pozitív szám is van a listában.")
+
+print("Alap lista:", numbers)
