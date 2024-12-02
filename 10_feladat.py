54d8f25d3344244cf9d5194c82f4da2df8d99e4b
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2024-12-02 09:30:18 +0100

    tiz

---
+++ 10_feladat.py
@@ -0,0 +1,19 @@
+import random
+
+numbers = [random.randint(-60, 100) for _ in range(50)]
+
+print("10. Igaz-e, hogy a sorozat szigorúan monoton növekvő?")
+
+monoton = True
+
+for i in range(1, len(numbers)):
+    if numbers[i] <= numbers[i - 1]:
+        monoton = False
+        break
+
+if monoton:
+    print("Igen, a sorozat szigorúan monoton növekvő.")
+else:
+    print("Nem, a sorozat nem szigorúan monoton növekvő.")
+
+print("Alap lista:", numbers)
