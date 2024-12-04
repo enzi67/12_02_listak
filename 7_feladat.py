bcf76ed0fd727defc95347cd1dd033367e35d387
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2024-12-04 13:24:47 +0100

    arriba

--- 7_feladat.py
+++ 7_feladat.py
@@ -1,6 +1,6 @@
 import random
 
-numbers = [random.randint(-60, 100) for _ in range(50)]
+numbers = [random.randint(-60, 100) for numero in range(50)]
 
 print("7. Mennyi a sorozatban található egyik legkisebb szám indexe?")
 
