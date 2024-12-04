8b2c9c75f8ab71526fe0caf7c8d9e3c477e86b75
Cseri Inez <cseri.inez@diak.szbi-pg.hu>
2024-12-04 13:22:38 +0100

    Update 1_feladat.py

--- 1_feladat.py
+++ 1_feladat.py
@@ -1,7 +1,7 @@
 import random
 import math
 
-numbers = [random.randint(-60, 100) for _ in range(50)]
+numbers = [random.randint(-60, 100) for num in range(50)]
 
 print("1. Mennyi a sorozatban található számok szorzata?")
 product_of_numbers = math.prod(numbers)
